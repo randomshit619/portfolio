68337d55c3b63af24e6bd10303d82ad8c1707292
randomshit619 <163145773+randomshit619@users.noreply.github.com>
2024-03-12 19:24:10 +0545

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using portfolio.Models;
+using System.Diagnostics;
+
+namespace portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            return View();
+        }
+
+        public IActionResult Services() {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
